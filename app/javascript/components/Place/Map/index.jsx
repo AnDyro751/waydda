bfb8cb5fb1787cdb9d7de532747a480f17c4f60e
Angel Mendez <angelmendezz751@gmail.com>
2020-08-07 00:15:37 -0500

    feat(place): Update place address in frontend

--- app/javascript/components/Place/Map/index.jsx
+++ app/javascript/components/Place/Map/index.jsx
@@ -1,10 +1,9 @@
 import React, {useState, useEffect, useRef} from 'react';
 import mapboxgl from 'mapbox-gl'
-import ReactMapGL from 'react-map-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYW5keXJvaG0iLCJhIjoiY2p6NmRldzJjMGsyMzNpbjJ0YjZjZjV5NSJ9.SeHsvxUe4-pszVk0B4gRAQ';
 
-export default function MapComponent({center, marker}) {
+export default function MapComponent({center, marker, draggable = false, onDrag}) {
 
     const mapRef = useRef();
     const [error, setError] = useState(false);
@@ -24,9 +23,13 @@ export default function MapComponent({center, marker}) {
                 center: center,
                 zoom: 14,
             });
-            new mapboxgl.Marker()
+            const dragMarker = new mapboxgl.Marker()
                 .setLngLat(marker)
-                .addTo(map);
+                .addTo(map).setDraggable(draggable);
+            dragMarker.on("dragend", (e) => {
+                let lngLat = dragMarker.getLngLat();
+                onDrag(lngLat)
+            })
             setError(false);
         } catch (e) {
             console.log("ERROR MAP", e)
@@ -40,12 +43,8 @@ export default function MapComponent({center, marker}) {
         )
     }
     return (
-        <div className="grid grid-cols-12 my-6 relative z-0">
-            <div className="col-span-12 h-56 md:h-xxl m-0 p-0 relative overflow-hidden">
-                {/*<ReactMapGL*/}
-                {/*    {...viewport}*/}
-                {/*    onViewportChange={(nviewport) => setViewport(nviewport)}*/}
-                {/*/>*/}
+        <div className="grid grid-cols-12 my-6 relative z-0 h-full">
+            <div className="col-span-12 h-full m-0 p-0 relative overflow-hidden">
                 <div className="h-full bg-gray-400 absolute w-full top-0 left-0 right-0 bottom-0" ref={mapRef}/>
             </div>
         </div>
--- app/javascript/components/dashboard/DashboardPlacePresentation.jsx
+++ app/javascript/components/dashboard/DashboardPlacePresentation.jsx
@@ -1,20 +1,60 @@
-import React, {Suspense} from "react"
+import React, {useState} from "react"
 import PropTypes from "prop-types"
 import GetImageUrl from "../../lib/getImageUrl";
+import MapComponent from "../Place/Map";
 
 export default function DashboardPlacePresentation({place, id}) {
+    const [defaultPlace, setDefaultPlace] = useState(place)
+    if (!place) {
+        return (<h1>Ha ocurrido un error</h1>)
+    }
+
+    const searchAddress = async (address, limit = 5) => {
+        const link = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw&cachebuster=1596775236930&autocomplete=true&country=mx&types=country%2Clocality%2Cneighborhood%2Cdistrict%2Cregion%2Cpostcode%2Caddress&limit=${limit}`
+        try {
+            const results = await (await fetch(link)).json()
+            return results.features;
+        } catch (e) {
+            return []
+        }
+
+    }
+
+    const handleDrag = async (e) => {
+        console.log(e);
+        try {
+            let results = await searchAddress(`${e.lng},${e.lat}`, 1);
+            if (results.length > 0) {
+                setDefaultPlace({...defaultPlace, coordinates: [e.lng, e.lat], address: results[0].place_name})
+            }
+        } catch (e) {
+            console.log("ERR", e)
+        }
+    }
+
     return (
         <>
-            <img className="h-10 w-10" src={GetImageUrl({publicId: place.photo, height: 50, width: 50})}
+            <h1 className="text-2xl font-bold">{place.name}</h1>
+            <h3 className="">{defaultPlace.address}</h3>
+            <h5><strong>Coordenadas</strong>: {defaultPlace.coordinates} </h5>
+            <img className="h-10 w-10" src={GetImageUrl({publicId: defaultPlace.photo, height: 50, width: 50})}
                  alt="Profile picture"/>
             {place.status !== "pending" &&
             <button id="select-files" data-model="place" data-slug={id} data-attribute="photo">Seleccionar
                 archivos</button>
             }
+            <div className="h-64 w-full">
+                <MapComponent
+                    draggable={true}
+                    center={[-99.134080, 19.426128]}
+                    marker={[-99.134080, 19.426128]}
+                    onDrag={handleDrag}
+                />
+            </div>
         </>
     )
 }
 DashboardPlacePresentation.propTypes = {
-  id: PropTypes.string.isRequired,
-  place: PropTypes.object.isRequired
+    id: PropTypes.string.isRequired,
+    place: PropTypes.object.isRequired
 }
