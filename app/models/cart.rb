6dcc06141ec1d738dc8fb87021705b2f6f618fbb
Angel Mendez <angelmendezz751@gmail.com>
2020-08-09 14:56:17 -0500

    refactor(Cart): Count cart items and show pricing

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,6 +5,15 @@ class Cart
   has_many :cart_items
   belongs_to :user, optional: true
 
+  def get_total
+    total = 0
+    items = self.cart_items.includes(:model)
+    items.each do |i|
+      total = total + (i.model.price * i.quantity)
+    end
+    total
+  end
+
   # @param [String] product_id
   # @return [Array] [response, total_products]
   def add_item(product_id)
@@ -12,7 +21,6 @@ class Cart
     return [false, nil] if product.nil?
     return [false, nil] if product.public_stock <= 0
     current_item = self.cart_items.find_by(model_id: product_id)
-    return [false, nil] if current_item.quantity > product.public_stock
     new_cart_quantity = self.quantity + 1
     if current_item.nil?
       begin
@@ -23,6 +31,7 @@ class Cart
         return [false, nil]
       end
     else
+      return [false, nil] if current_item.quantity > product.public_stock
       new_quantity = current_item.quantity + 1
       # puts "#{new_quantity}----------"
       if current_item.update(quantity: new_quantity)
