003cff101a466c4a8d5dd96641985b51c61b28a6
Angel Mendez <angelmendezz751@gmail.com>
2020-08-10 16:48:14 -0500

    feat(Frontend/Header): webPack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    purge: {
+        content: [
+            './app/views/**/*.{js,jsx,ts,tsx,html,html.erb}',
+            './app/javascript/components/**/*.{js,jsx,ts,tsx}'
+        ]
+    },
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
 }
