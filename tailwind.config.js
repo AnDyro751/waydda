d10411d6f0d7679a143241fe02629ae5e239b763
Angel Mendez <angelmendezz751@gmail.com>
2020-08-11 16:10:08 -0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     purge: [
         './app/views/**/*.{js,jsx,ts,tsx,html,html.erb}',
-        './app/javascript/components/**/*.{js,jsx,ts,tsx}'
+        './app/javascript/bundles/**/*.{js,jsx,ts,tsx}'
     ],
     theme: {
         extend: {},
