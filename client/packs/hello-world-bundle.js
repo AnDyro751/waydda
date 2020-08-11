c27319271762d9d54d11cb0465bd667e6e896085
Angel Mendez <angelmendezz751@gmail.com>
2020-08-11 15:12:03 -0500

    refactor: Remove client directory

--- client/packs/hello-world-bundle.js
+++
@@ -1,6 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-import Simple from '../bundles/components/Headers/Simple'
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-    Simple,
-});
