e69cefdb4024108063b9ada1746ba90d528df379
Angel Mendez <angelmendezz751@gmail.com>
2020-08-13 09:45:58 -0500

    refactor: El caché de rails evita que el pinche contenido cambie, me lleva la chaisn

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,7 +16,7 @@ class PlacesController < ApplicationController
   # GET /places/1.json
   def show
     @products = @place.products.paginate(page: params[:page], per_page: 30)
-    fresh_when @place
+    # fresh_when @place
   end
 
   private
