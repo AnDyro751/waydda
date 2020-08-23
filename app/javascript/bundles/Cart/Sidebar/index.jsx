0a0e4cafad70f3de6c62138eabca33ba19970980
Angel Mendez <angelmendezz751@gmail.com>
2020-08-23 14:06:09 -0500

    feat(Place): Overlay class

--- app/javascript/bundles/Cart/Sidebar/index.jsx
+++ app/javascript/bundles/Cart/Sidebar/index.jsx
@@ -4,7 +4,6 @@ import classNames from 'classnames'
 import getDefaultHeaders from "../../../lib/getDefaultHeaders";
 import {RiCloseLine} from 'react-icons/ri'
 import ShowCartItems from "../ShowCartItems";
-import SelectAddress from "../../Addresses/Select";
 import AddressModal from "../../Addresses/Modal";
 import ReactDOM from 'react-dom'
 
@@ -74,56 +73,69 @@ const Sidebar = ({handleOpen, open}) => {
     })
 
     const sidebarParentClass = classNames({
-        "fixed transform top-0 w-2/6 z-10 flex right-0 bottom-0 h-screen justify-end transform transition duration-500 delay-75 ease-in-out text-left": true,
+        "fixed transform top-0 w-2/6 z-20 flex right-0 bottom-0 h-screen justify-end transform transition duration-500 delay-75 ease-in-out text-left": true,
         "translate-x-full": !open,
         "translate-x-0": open,
     })
-    if (error) {
-        return null;
-    }
+    const overlayClass = classNames({
+        "fixed transform z-10 top-0 w-full flex right-0 bottom-0 h-screen justify-end w-full bg-black sidebar-overlay": true,
+        "opacity-0 invisible": !open,
+        "opacity-50 visible": open
+    })
+    // if (error) {
+    //     return null;
+    // }
     return ReactDOM.createPortal(
-        <div
-            className={sidebarParentClass}>
+        <>
             <div
-                className={sidebarClass}>
-                <div className="grid grid-cols-12 relative">
-                    <div className="col-span-12 p-4 border-b relative"
-                         style={{
-                             backgroundColor: "#fafafa"
-                         }}
-                    >
-                        <div className="flex w-full items-center justify-between flex-wrap">
-                            <div className="w-8/12">
-                                <h3 className="text-lg font-bold">Carrito</h3>
-                            </div>
-                            <div className="w-2/12 p-2 cursor-pointer flex justify-end"
-                                 onClick={() => {
-                                     handleOpen(false);
-                                 }}
-                            >
-                                <RiCloseLine size={20} className="text-black"/>
-                            </div>
-                            {
-                                !loading &&
-                                <div className="w-full py-4">
-                                    <h4 className="text-sm text-gray-800 mb-4">Entregar en:</h4>
-                                    <AddressModal
-                                        current_address={currentAddress}
-                                        modalOpen={false}
-                                    />
+                onClick={() => {
+                    handleOpen(false);
+                }}
+                className={overlayClass}>
+            </div>
+            <div
+                className={sidebarParentClass}>
+                <div
+                    className={sidebarClass}>
+                    <div className="grid grid-cols-12 relative">
+                        <div className="col-span-12 p-4 border-b relative"
+                             style={{
+                                 backgroundColor: "#fafafa"
+                             }}
+                        >
+                            <div className="flex w-full items-center justify-between flex-wrap">
+                                <div className="w-8/12">
+                                    <h3 className="text-lg font-bold">Carrito</h3>
                                 </div>
-                            }
+                                <div className="w-2/12 p-2 cursor-pointer flex justify-end"
+                                     onClick={() => {
+                                         handleOpen(false);
+                                     }}
+                                >
+                                    <RiCloseLine size={20} className="text-black"/>
+                                </div>
+                                {
+                                    !loading &&
+                                    <div className="w-full py-4">
+                                        <h4 className="text-sm text-gray-800 mb-4">Entregar en:</h4>
+                                        <AddressModal
+                                            current_address={currentAddress}
+                                            modalOpen={false}
+                                        />
+                                    </div>
+                                }
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="col-span-12">
-                        <ShowCartItems
-                            withImages={false}
-                            items={items}
-                        />
+                        <div className="col-span-12">
+                            <ShowCartItems
+                                withImages={false}
+                                items={items}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
         , document.querySelector("#portal"))
 }
