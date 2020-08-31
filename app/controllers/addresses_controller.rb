fd782432a6b1af57ac552d874f8d3fea981751c3
Angel Mendez <angelmendezz751@gmail.com>
2020-08-30 21:05:46 -0500

    feat(Address): Actualiza y crea direcciones

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -55,6 +55,7 @@ class AddressesController < ApplicationController
     respond_to do |format|
       if @address.update(address_params)
         format.html { redirect_to @address, notice: 'Address was successfully updated.' }
+        format.js
         format.json { render :show, status: :ok, location: @address }
       else
         format.html { render :edit }
