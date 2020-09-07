2c339c24688ff0d076e4019ad6f4f7c33166c882
Angel Mendez <angelmendezz751@gmail.com>
2020-09-06 21:29:26 -0500

    feat(Dashboard): Mostramos las opciones especificas para cada tipo de empresa

--- app/javascript/packs/places/connect.js
+++ app/javascript/packs/places/connect.js
@@ -1,13 +1,16 @@
 import getDefaultHeaders from "../../lib/getDefaultHeaders";
 
-var connectElement = document.querySelector("#get-to-my-dashboard");
+var connectElement = document.querySelector(".get-to-my-dashboard");
 if (connectElement) {
     connectElement.addEventListener("click", async () => {
         try {
             connectElement.innerHTML = "Cargando...";
             connectElement.disabled = true;
             var response = await (
-                (await fetch("/dashboard/settings/payments/connect/link", {method: "POST", headers: getDefaultHeaders()})).json()
+                (await fetch("/dashboard/settings/payments/connect/link", {
+                    method: "POST",
+                    headers: getDefaultHeaders()
+                })).json()
             )
             if (response.errors) {
                 connectElement.disabled = false;
