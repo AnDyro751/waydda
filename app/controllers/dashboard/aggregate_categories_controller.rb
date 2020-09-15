17783035ce137cc688b246da0f242470849f2aed
Angel Mendez <angelmendezz751@gmail.com>
2020-09-15 03:34:45 -0500

    feat(Place): Añadimos las acciones necesarias para el crud de las categorías

--- app/controllers/dashboard/aggregate_categories_controller.rb
+++ app/controllers/dashboard/aggregate_categories_controller.rb
@@ -1,9 +1,29 @@
 class Dashboard::AggregateCategoriesController < ApplicationController
   layout "dashboard"
+  before_action :authenticate_user!
+  before_action :set_my_place
+  before_action :set_product
+  before_action :set_aggregate_category, only: [:show, :update, :edit, :destroy]
 
   def new
   end
 
   def edit
   end
+
+  def show
+
+  end
+
+  private
+
+  def set_product
+    @product = @place.products.find_by(slug: params["product_id"])
+    not_found if @product.nil?
+  end
+
+  def set_aggregate_category
+    (@aggregate_category = @product.aggregate_categories.find_by(id: params["id"])) or not_found
+  end
+
 end
