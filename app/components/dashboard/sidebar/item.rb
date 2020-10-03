b6d1354ad8c7d74191dfbf10a209b323c254e653
Angel Mendez <angelmendezz751@gmail.com>
2020-10-03 12:21:53 -0500

    Feat(Dashboard): Cambios en el sidebar

--- app/components/dashboard/sidebar/item.rb
+++ app/components/dashboard/sidebar/item.rb
@@ -1,7 +1,8 @@
 class Dashboard::Sidebar::Item < ViewComponent::Base
-  def initialize(text:, to:, active_controller:)
+  def initialize(text:, to:, active_controller:, active_actions: nil)
     @text = text
     @to = to
     @active_controller = active_controller
+    @active_actions = active_actions
   end
 end
