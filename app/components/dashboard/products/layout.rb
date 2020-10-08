973dd03089e6c41aa7c604105ed2ba97d74cead7
Angel Mendez <angelmendezz751@gmail.com>
2020-10-07 20:27:18 -0500

    feat(Dashboard): Cambiamos la estructura del layout para poder aceptar el full page o un min page

--- app/components/dashboard/products/layout.rb
+++ app/components/dashboard/products/layout.rb
@@ -1,5 +1,6 @@
 class Dashboard::Products::Layout < ViewComponent::Base
-  def initialize(product:)
+  def initialize(product:, full_page: false)
     @product = product
+    @full_page = full_page
   end
 end
