8f14c404bd4ee291b2401c9a18aa889a2401691d
Angel Mendez <angelmendezz751@gmail.com>
2020-10-07 23:33:01 -0500

    feat(Dashboard): Mostramos el form, solo queda hacer el refactor y mostrar bien los inputs

--- app/components/dashboard/aggregate_categories/List/simple.rb
+++ app/components/dashboard/aggregate_categories/List/simple.rb
@@ -1,8 +1,9 @@
 class Dashboard::AggregateCategories::List::Simple < ViewComponent::Base
 
-  def initialize(product:, aggregate_category:)
+  def initialize(product:, aggregate_category:, f:)
     @product = product
     @aggregate_category = aggregate_category
+    @f = f
   end
 
 end
