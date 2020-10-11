e0435e60f8eafd6652d661cc16cacbd1daf0422c
Angel Mendez <angelmendezz751@gmail.com>
2020-10-11 13:00:19 -0500

    feat(Dashboard): Validate product.rb and test product_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,30 @@
 require 'rails_helper'
 RSpec.describe Product, type: :model do
 
-  it { should validate_presence_of(:name) }
+  describe "Fields model" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:price) }
+    it { should have_field(:name).of_type(String) }
+    it { should have_field(:description).of_type(String) }
+    it { should have_field(:price).of_type(Float).with_default_value_of(0) }
+    it { should have_field(:slug).of_type(String) }
+    it { should have_field(:unlimited).of_type(Mongoid::Boolean) }
+    it { should have_field(:status).of_type(String) }
+    it { should have_field(:photo).of_type(String).with_default_value_of("places/default.png") }
+    it { should have_field(:sku).of_type(String) }
+    it { should have_field(:weight).of_type(Float) }
+    it { should belong_to(:place).of_type(Place) }
+    it { should have_and_belong_to_many(:items) }
+    it { should accept_nested_attributes_for(:aggregate_categories) }
 
+    it { should validate_length_of(:name).within(4..30) }
+    it { should validate_numericality_of(:price).greater_than(0) }
+    it { should validate_inclusion_of(:status).to_allow("active", "inactive") }
+    it { should validate_inclusion_of(:unlimited).to_allow(true, false) }
+    it { should validate_numericality_of(:quantity).greater_than(0) }
+    it { should validate_inclusion_of(:quantity_measure).to_allow("kg", "grm", "oz", "ml", "l", "m2", "m3", "pzas") }
+
+  end
   context "Valid aggregates" do
 
     let(:place) { FactoryBot.create(:free_valid_place) }
@@ -43,15 +65,15 @@ RSpec.describe Product, type: :model do
         it 'should be return false in valid_sale?' do
           valid_product.update(unlimited: false)
           valid_product.reload
-          expect(valid_product.valid_sale?(quantity: 4)).to eq(false)
+          expect { valid_product.valid_sale?(quantity: 4) }.to raise_error(RuntimeError)
         end
 
         it 'should be return false when unlimited is false and intent buy -1 item' do
           valid_product.update(unlimited: false)
-          expect(valid_product.reload.valid_sale?(quantity: -1)).to eq(false)
+          expect { valid_product.reload.valid_sale?(quantity: -1) }.to raise_error(RuntimeError)
         end
 
-        it { expect(valid_product.valid_sale?(quantity: 0)).to eq(false) }
+        it { expect { valid_product.valid_sale?(quantity: 0) }.to raise_error(RuntimeError) }
 
         it 'should be return true when public stock is zero and unlimited is true' do
           valid_product.update(unlimited: true)
@@ -60,7 +82,7 @@ RSpec.describe Product, type: :model do
 
         it 'should be return false in valid_sale? when unlimited is false and public_stock is 3 and intent buy 4 items' do
           valid_product.update(public_stock: 3, unlimited: false)
-          expect(valid_product.reload.valid_sale?(quantity: 4)).to eq(false)
+          expect { valid_product.reload.valid_sale?(quantity: 4) }.to raise_error(RuntimeError)
         end
 
         it 'should be return true in valid_sale? when unlimited is false and public_stock is 3 and intent buy 3 items' do
@@ -77,10 +99,10 @@ RSpec.describe Product, type: :model do
         }
 
         before(:each) {
-          aggregate_categories_1.aggregates << FactoryBot.create(:aggregate, aggregate_category: aggregate_categories_1, name: "Demo 1")
-          aggregate_categories_1.aggregates << FactoryBot.create(:aggregate, aggregate_category: aggregate_categories_1, name: "Demo 2")
-          aggregate_categories_2.aggregates << FactoryBot.create(:aggregate, aggregate_category: aggregate_categories_2)
-          aggregate_categories_2.aggregates << FactoryBot.create(:aggregate, aggregate_category: aggregate_categories_2)
+          aggregate_categories_1.aggregates.new(attributes_for :aggregate)
+          aggregate_categories_1.aggregates.new(attributes_for :aggregate)
+          aggregate_categories_2.aggregates.new(attributes_for :aggregate)
+          aggregate_categories_2.aggregates.new(attributes_for :aggregate)
         }
 
         let(:valid_params) {
@@ -106,18 +128,17 @@ RSpec.describe Product, type: :model do
 
         it 'should be return false because one id is incorrect' do
           aggregates = [{id: "false_id"}, {id: aggregate_categories_3_not_required}]
-          expect(valid_product.valid_aggregates_sale?(aggregates: aggregates)).to eq(false)
+          expect { valid_product.valid_aggregates_sale?(aggregates: aggregates) }.to raise_error(RuntimeError)
         end
 
         it 'should be return false in valid_aggregates_sale when product is inactive' do
           aggregates = [{id: aggregate_categories_1}, {id: aggregate_categories_2}, {id: aggregate_categories_3_not_required}]
-          expect(valid_product.valid_aggregates_sale?(aggregates: aggregates)).to eq(false)
+          expect { valid_product.valid_aggregates_sale?(aggregates: aggregates) }.to raise_error(RuntimeError)
         end
 
         it 'should be true' do
           expect(valid_product.valid_aggregates_sale?(aggregates: valid_params)).to eq(true)
         end
-
 
 
       end
