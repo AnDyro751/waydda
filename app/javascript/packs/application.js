0480e6e6ac267f516495f759498a1cf61aa11b96
Angel Mendez <angelmendezz751@gmail.com>
2020-10-20 18:55:13 -0500

    feat(Dashboard): Arreglamos los multiples loads

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -209,44 +209,44 @@ document.addEventListener("turbolinks:load", () => {
 
     window.Toastify = Toastify;
 
-    function logElementEvent(eventName, element) {
-        console.log(Date.now(), eventName, element.getAttribute("data-src"));
-    }
-
-    var callback_enter = function (element) {
-        logElementEvent("🔑 ENTERED", element);
-    };
-    var callback_exit = function (element) {
-        logElementEvent("🚪 EXITED", element);
-    };
-    var callback_loading = function (element) {
-        logElementEvent("⌚ LOADING", element);
-    };
-    var callback_loaded = function (element) {
-        logElementEvent("👍 LOADED", element);
-    };
-    var callback_error = function (element) {
-        logElementEvent("💀 ERROR", element);
-        element.src =
-            "https://via.placeholder.com/440x560/?text=Error+Placeholder";
-    };
-    var callback_finish = function () {
-        logElementEvent("✔️ FINISHED", document.documentElement);
-    };
-    var callback_cancel = function (element) {
-        logElementEvent("🔥 CANCEL", element);
-    };
+    // function logElementEvent(eventName, element) {
+    //     console.log(Date.now(), eventName, element.getAttribute("data-src"));
+    // }
+    //
+    // var callback_enter = function (element) {
+    //     logElementEvent("🔑 ENTERED", element);
+    // };
+    // var callback_exit = function (element) {
+    //     logElementEvent("🚪 EXITED", element);
+    // };
+    // var callback_loading = function (element) {
+    //     logElementEvent("⌚ LOADING", element);
+    // };
+    // var callback_loaded = function (element) {
+    //     logElementEvent("👍 LOADED", element);
+    // };
+    // var callback_error = function (element) {
+    //     logElementEvent("💀 ERROR", element);
+    //     element.src =
+    //         "https://via.placeholder.com/440x560/?text=Error+Placeholder";
+    // };
+    // var callback_finish = function () {
+    //     logElementEvent("✔️ FINISHED", document.documentElement);
+    // };
+    // var callback_cancel = function (element) {
+    //     logElementEvent("🔥 CANCEL", element);
+    // };
 
     window.customLazyLoad = new LazyLoad({
         // Assign the callbacks defined above
         elements_selector: ".lazy",
-        callback_enter: callback_enter,
-        callback_exit: callback_exit,
-        callback_cancel: callback_cancel,
-        callback_loading: callback_loading,
-        callback_loaded: callback_loaded,
-        callback_error: callback_error,
-        callback_finish: callback_finish
+        // callback_enter: callback_enter,
+        // callback_exit: callback_exit,
+        // callback_cancel: callback_cancel,
+        // callback_loading: callback_loading,
+        // callback_loaded: callback_loaded,
+        // callback_error: callback_error,
+        // callback_finish: callback_finish
     });
 
     try {
