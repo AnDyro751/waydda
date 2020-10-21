3b5f40125f5a56b9b9786ffd89432193142bffa3
Angel Mendez <angelmendezz751@gmail.com>
2020-10-21 15:24:39 -0500

    feat(Dashboard): Agregamos el titulo al inicio de sesión y eliminamos el cover de departaments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
-
   layout "business", only: [:business, :pricing]
   before_action :set_price
+
   def index
+    set_meta_tags title: "Waydda México",
+                  description: "Waydda México"
     @places = Place.where(status: "active").order_by(created_at: :desc).paginate(page: params[:page], per_page: 20)
   end
 
