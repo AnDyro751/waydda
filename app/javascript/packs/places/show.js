e323072ad4266dc451e43bdc234f88bacf9c5cd2
Angel Mendez <angelmendezz751@gmail.com>
2020-10-22 17:24:36 -0500

    feat(Place): Cambiamos el gap por un space-x-5

--- app/javascript/packs/places/show.js
+++ app/javascript/packs/places/show.js
@@ -1,16 +1,20 @@
 var ele = document.getElementById("scroll-container");
 document.addEventListener("turbolinks:load", function () {
-    var progressBar = new Turbolinks.ProgressBar()
-    document.querySelector("#get-my-cart").addEventListener("ajax:beforeSend", function () {
-        progressBar.setValue(0);
-        progressBar.show();
-    });
-    document.querySelector("#get-my-cart").addEventListener("ajax:success", function () {
-        if (progressBar) {
-            progressBar.setValue(1);
-            progressBar.hide();
-        }
-    });
+    var cartElement = document.querySelector("#get-my-cart");
+    if (cartElement) {
+        cartElement.addEventListener("ajax:beforeSend", function () {
+            progressBar.setValue(0);
+            progressBar.show();
+        });
+        cartElement.addEventListener("ajax:complete", function () {
+            if (progressBar) {
+                progressBar.setValue(1);
+                progressBar.hide();
+            }
+        });
+        var progressBar = new Turbolinks.ProgressBar()
+
+    }
     var showModal = document.querySelector("meta[name=show-modal]");
 
     if (showModal) {
