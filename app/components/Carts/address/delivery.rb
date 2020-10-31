b094eaea5fa2ca85dd5edf6928592b7d04f74daf
Angel Mendez <angelmendezz751@gmail.com>
2020-10-31 13:44:32 -0600

    feat(Cart): Delivery options

--- app/components/Carts/address/delivery.rb
+++ app/components/Carts/address/delivery.rb
@@ -1,7 +1,8 @@
 class Carts::Address::Delivery < ViewComponent::Base
-  def initialize(place:, address:, editable: true)
+  def initialize(place:, address:, editable: true, current_cart: nil)
     @place = place
     @address = address
     @editable = editable
+    @current_cart = current_cart
   end
 end
