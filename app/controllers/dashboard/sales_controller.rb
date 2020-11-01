adb30e4231394b0e0b5f6fceac20f40774eb523c
Angel Mendez <angelmendezz751@gmail.com>
2020-11-01 12:26:17 -0600

    feat(Cart): Agregamos otros filtros de busqueda y un catch para retornar cuando el filtro no es correcto

--- app/controllers/dashboard/sales_controller.rb
+++ app/controllers/dashboard/sales_controller.rb
@@ -7,13 +7,22 @@ class Dashboard::SalesController < ApplicationController
     set_meta_tags title: "Todas las ventas | Panel de control",
                   description: "Todas las ventas - Panel de control"
     add_breadcrumb "Mis ventas", dashboard_sales_path
-    @sales = @place.orders.includes(:order_items).paginate(page: params[:page], per_page: 30)
     if params[:filter].nil?
+      @sales = @place.orders.includes(:order_items).paginate(page: params[:page], per_page: 30)
       add_breadcrumb "Todas las ventas", dashboard_sales_path
     elsif params[:filter] === "process"
+      @sales = @place.orders.where(status: "in_process").includes(:order_items).paginate(page: params[:page], per_page: 30)
       add_breadcrumb "Pedidos en proceso", dashboard_sales_path
     elsif params[:filter] === "pending"
+      @sales = @place.orders.where(status: "pending").includes(:order_items).paginate(page: params[:page], per_page: 30)
       add_breadcrumb "Pedidos pendientes", dashboard_sales_path
+    elsif params[:filter] === "cancelled"
+      @sales = @place.orders.where(status: "cancelled").includes(:order_items).paginate(page: params[:page], per_page: 30)
+      add_breadcrumb "Pedidos cancelados", dashboard_sales_path
+    elsif params[:filter] === "sent"
+      @sales = @place.orders.where(status: "sent").includes(:order_items).paginate(page: params[:page], per_page: 30)
+    else
+      redirect_to dashboard_sales_path, notice: "No se ha encontrado el filtro de búsqueda"
     end
     if params[:page].present?
       unless params[:page] === "1"
