8e0f2500bcb2a3c5a55c3fd171c55ff2069d9434
Angel Mendez <angelmendezz751@gmail.com>
2020-11-03 19:14:47 -0600

    feat(Dashboard): Creamos el svg de la orden
    Signed-off-by: Angel Mendez <angelmendezz751@gmail.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -136,6 +136,21 @@ class Order
     end
   end
 
+
+  def create_qrcode
+    qrcode = RQRCode::QRCode.new("http://github.com/")
+
+    # NOTE: showing with default options specified explicitly
+    svg = qrcode.as_svg(
+        offset: 0,
+        color: '000',
+        shape_rendering: 'crispEdges',
+        module_size: 6,
+        standalone: true
+    )
+    return svg
+  end
+
   private
 
   def create_order_callback
