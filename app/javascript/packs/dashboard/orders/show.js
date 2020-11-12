bdc0333775c7db2c85c1b8e0f6611edcaa75779c
Angel Mendez <angelmendezz751@gmail.com>
2020-11-12 14:23:18 -0600

    feat(Emails): Mostramos el mapa en el show de la vista de order
    Signed-off-by: Angel Mendez <angelmendezz751@gmail.com>

--- app/javascript/packs/dashboard/orders/show.js
+++ app/javascript/packs/dashboard/orders/show.js
@@ -1,5 +1,7 @@
 import mapboxgl from "mapbox-gl";
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2F5ZGRhIiwiYSI6ImNrZzYwZWJiYzB6bjMycW5udmd1NHNscDAifQ.wkmzM9Mh8XyPXZ8BgpyJXg';
+
 if (document.querySelector("#map")) {
     var lat = parseFloat(document.querySelector("#map").dataset["mapLat"]);
     var lng = parseFloat(document.querySelector("#map").dataset["mapLng"]);
@@ -10,4 +12,7 @@ if (document.querySelector("#map")) {
         center: [lng ? lng : -99.212594, lat ? lat : 19.455834], // starting position [lng, lat]
         zoom: 16 // starting zoom
     });
+    var currentMarker = new mapboxgl.Marker()
+        .setLngLat([lng, lat])
+        .addTo(window.current_map).setDraggable(false);
 }
